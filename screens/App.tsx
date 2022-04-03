cdecdbc8014e82fb83120807e510a2208863fe88
yuki-hinata <bigisland20001010@gmail.com>
2022-04-03 13:03:43 +0900

    feat: eslint追加

--- screens/App.tsx
+++ screens/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { NativeBaseProvider } from 'native-base';
+import React from 'react'
+import { NativeBaseProvider } from 'native-base'
 
-import useCachedResources from '../hooks/useCachedResources';
-import useColorScheme from '../hooks/useColorScheme';
-import { RootNavigator } from '../navigation/RootNavigator';
+import useCachedResources from '../hooks/useCachedResources'
+import { RootNavigator } from '../navigation/RootNavigator'
 
-export default function App() {
+export default function App () {
   // わざわざこれでやらなくても、isLoadingとsetIsloadingで判断すればよいのでは。
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <NativeBaseProvider>
         <RootNavigator />
       </NativeBaseProvider>
-    );
+    )
   }
 }
